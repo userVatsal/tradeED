d14b506176a8648208af08bc1386246715040c7c
Vatsal Pareshkumar <127995002+userVatsal@users.noreply.github.com>
2025-02-09 13:45:44 +0000

    Update header.inc.php

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,1 +1,123 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title><?php echo $webpage->getTitle(); ?></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../images/logo-white.png" type="image/x-icon">
+    <!-- Bootstrap CSS 5.3.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php if ($webpage->getFlatPickr()) { ?>
+        <!-- FlatPickr CSS -->
+        <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/dark.css">
+    <?php } ?>
+    <!-- Other CSS -->
+    <link rel="stylesheet" href="../styles/mystyles.css">
+</head>
+
+<body data-bs-theme="">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary nav-pills">
+        <div class="container-fluid">
+            <img class="navbar-brand" src="../images/logo-white.png" height="60px" draggable="false" />
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Nav links -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $webpage->getActive("home"); ?>" href="../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $webpage->getActive("about"); ?>" href="../pages/about.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $webpage->getActive("hotel"); ?>" href="../pages/hotel.php">Hotel</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $webpage->getActive("ticket"); ?>" href="../pages/ticket.php">Ticket</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle <?php echo $webpage->getActive("discover"); ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Discover
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item <?php echo $webpage->getActive("animal"); ?>" href="../pages/animal.php">Animal</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item <?php echo $webpage->getActive("facility"); ?>" href="../pages/facility.php">Facility</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- Button -->
+                <div class="d-flex gap-2 my-1 me-2 d-flex justify-content-center">
+                    <!-- Admin -->
+                    <?php if (isset($_COOKIE["isAdmin"])) { ?>
+                        <a href="../pages/admin.php" class="btn btn-light">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                            </svg>
+                        </a>
+                    <?php } ?>
+                    <?php if (isset($_COOKIE["userID"])) { ?>
+                        <!-- Loyalty Points -->
+                        <a href="../pages/loyalty.php" class="btn btn-light">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                            </svg>
+                        </a>
+                        <!-- Cart -->
+                        <a href="../pages/payment.php" class="btn btn-light">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-basket2-fill" viewBox="0 0 16 16">
+                                <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1" />
+                            </svg>
+                        </a>
+                        <!-- Booking -->
+                        <a href="../pages/bookings.php" class="btn btn-light">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-calendar-event-fill" viewBox="0 0 16 16">
+                                <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2m-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5" />
+                            </svg>
+                        </a>
+                    <?php } ?>
+                    <!-- Theme Toggle -->
+                    <button class="btn btn-light" id="themeBtn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16">
+                            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
+                        </svg>
+                    </button>
+                </div>
+                <!-- Drop Down -->
+                <div class="navbar-nav me-2">
+                    <li class="nav-item dropdown text-center">
+                        <a class="nav-link dropdown-toggle button text-bg-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
+                        <ul class="dropdown-menu dropdown-menu-end mt-1">
+                            <?php if (isset($_COOKIE["userID"])) { ?>
+                                <li><a class="dropdown-item" href="../pages/profile.php">Account</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="../pages/profile.php?type=logout">Logout</a></li>
+                            <?php } else { ?>
+                                <li><a class="dropdown-item" href="../pages/account.php?type=login">Login</a></li>
+                                <li><a class="dropdown-item" href="../pages/account.php?type=register">Register</a></li>
+                            <?php } ?>
+                        </ul>
+                    </li>
+                </div>
+            </div>
+        </div>
+    </nav>
 
